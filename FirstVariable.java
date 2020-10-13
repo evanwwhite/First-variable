61d83f3b54cbbbc17ac8ea03db1db9410d759bbf
Evan White <69662443+evanwwhite@users.noreply.github.com>
2020-10-13 13:25:05 -0500

    Add files via upload

---
+++ FirstVariable.java
@@ -0,0 +1,33 @@
+// Evan White
+import java.util.Calendar;
+public class FirstVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Calendar.getInstance().getTime() + "\n\n");
+		
+	int     aInteger = 20;
+	double  aDouble = 12.32;
+	boolean aBoolean = false;
+	char    aChar = 'X';
+	String  aString = "Crocodile";
+
+	System.out.println("aInteger:  " + aInteger);
+	System.out.println("aDouble:  " + aDouble);
+	System.out.println("aBoolean:  " + aBoolean);
+	System.out.println("aChar:  " + aChar);
+	System.out.println("aString:  " + aString);
+
+	}
+
+}
+
+/*Thu Sep 10 13:51:58 CDT 2020
+
+
+aInteger:  20
+aDouble  12.32
+aBoolean  false
+aChar  X
+aString  Crocodile
+*/
